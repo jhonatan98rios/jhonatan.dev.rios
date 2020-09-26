676d4a07f3294cc2d91f65630bfb47b17b236c5e
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-09-26 04:39:12 -0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "57907501e8960039c2b7";
+/******/ 	var hotCurrentHash = "8e085ecc5d3a59689213";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
