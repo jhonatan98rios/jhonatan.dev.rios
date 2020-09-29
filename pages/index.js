d55ce330ae5e603ef10a42aa5574daaf8dde711c
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-09-29 08:35:21 -0300

    particcles global fix

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
+import { useEffect } from 'react';
 import Head from 'next/head'
+
 import Navbar from '../partials/Shared/Navbar'
 import Hero from '../partials/Home/Hero'
 import About from '../partials/Home/About'
 import Technologies from '../partials/Home/Technologies'
-/* import ProjectsBanner from '../partials/Home/ProjectsBanner' */
 import GitHub from '../partials/Shared/GitHub'
 import Footer from '../partials/Shared/Footer'
-
+import Particles from '../controllers/Particles'
+/* import ProjectsBanner from '../partials/Home/ProjectsBanner' */
 
 export default function Home() {
+
+  const particlesEl = React.createRef()
+
+  useEffect(()=>{
+    
+    const particles = new Particles(particlesEl.current, window.innerHeight, window.innerWidth)
+    particles.startParticles()
+
+  }, [])
+
   return (
     <div>
       <Head>
@@ -17,6 +29,7 @@ export default function Home() {
       </Head>
 
       <main className="transition">
+        <div ref={particlesEl} />
         <Navbar />
         <Hero />
         <About />
--- partials/Home/Hero/index.js
+++ partials/Home/Hero/index.js
@@ -1,17 +1,12 @@
 import styles from './styles.module.scss'
-import Particles from '../../../controllers/Particles'
 import TypeWriter from '../../../controllers/TypeWriter'
 import { useEffect } from 'react';
 
 export default function Hero() {
 
-  const particlesEl = React.createRef()
   const typeWriterEl = React.createRef()
   
   useEffect(()=>{
-    
-    const particles = new Particles(particlesEl.current, window.innerHeight, window.innerWidth)
-    particles.startParticles()
 
     const typeWriter = new TypeWriter()
     typeWriter.startWriter(typeWriterEl.current)
@@ -19,7 +14,7 @@ export default function Hero() {
   }, [])
 
   return (
-    <section className={styles.container} ref={particlesEl} id={"home-hero"}>
+    <section className={styles.container} id={"home-hero"}>
       <h1 className={`${styles.title} title`} ref={typeWriterEl}> 
         "As pessoas que são loucas o suficiente para achar que podem mudar o mundo, são aquelas que o mudam" (Steve Jobs)
       </h1>
