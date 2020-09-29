46639ab8e698bd76b45359114930cbb6d02eaaa0
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-09-29 09:26:37 -0300

    Canvas back to home

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,9 @@
-import { useEffect } from 'react';
-import Particles from '../controllers/Particles'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-
-  const particlesEl = React.createRef()
-
-  useEffect(()=>{
-    
-    const particles = new Particles(particlesEl.current, window.innerHeight, window.innerWidth)
-    particles.startParticles()
-
-  }, [])
   
   return (
-    <>
-      <div ref={particlesEl} />
-      <Component {...pageProps} />
-    </>
+    <Component {...pageProps} />
   )
 }
 
--- partials/Home/Hero/index.js
+++ partials/Home/Hero/index.js
@@ -1,20 +1,27 @@
 import styles from './styles.module.scss'
 import TypeWriter from '../../../controllers/TypeWriter'
+import Particles from '../../../controllers/Particles'
 import { useEffect } from 'react';
 
 export default function Hero() {
 
   const typeWriterEl = React.createRef()
+  const particlesEl = React.createRef()
+  
   
   useEffect(()=>{
+
+    const particles = new Particles(particlesEl.current, window.innerHeight, window.innerWidth)
+    particles.startParticles()
 
     const typeWriter = new TypeWriter()
     typeWriter.startWriter(typeWriterEl.current)
 
   }, [])
 
+
   return (
-    <section className={styles.container} id={"home-hero"}>
+    <section className={styles.container} id={"home-hero"} ref={particlesEl}>
       <h1 className={`${styles.title} title`} ref={typeWriterEl}> 
         "As pessoas que são loucas o suficiente para achar que podem mudar o mundo, são aquelas que o mudam" (Steve Jobs)
       </h1>
