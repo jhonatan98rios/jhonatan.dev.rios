eba4f20cff0c4c1528a0949c5f0758b7db2b4f6d
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-09-30 23:17:20 -0300

    canvas fix

--- partials/Home/Hero/index.js
+++ partials/Home/Hero/index.js
@@ -1,12 +1,17 @@
 import styles from './styles.module.scss'
+import Particles from '../../../controllers/Particles'
 import TypeWriter from '../../../controllers/TypeWriter'
 import { useEffect } from 'react';
 
 export default function Hero({content}) {
 
+  const particlesEl = React.createRef()
   const typeWriterEl = React.createRef()
   
   useEffect(()=>{
+
+    const particles = new Particles(particlesEl.current, window.innerHeight, window.innerWidth)
+    particles.startParticles()
 
     const typeWriter = new TypeWriter()
     typeWriter.startWriter(typeWriterEl.current)
@@ -14,7 +19,7 @@ export default function Hero({content}) {
   }, [])
 
   return (
-    <section className={styles.container} id={"home-hero"}>
+    <section className={styles.container} id={"home-hero"} ref={particlesEl}>
       <h1 className={`${styles.title} title`} ref={typeWriterEl}> 
         {content}
       </h1>
