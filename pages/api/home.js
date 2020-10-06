caf26ec7a206f9d2e2a2af0b465f2c027ea00c11
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-10-06 08:48:02 -0300

    Split sections of tecnologies

--- pages/api/home.js
+++ pages/api/home.js
@@ -8,76 +8,100 @@ export const home = (req, res) => {
     body: {
       hero: '"As pessoas que são loucas o suficiente para achar que podem mudar o mundo, são aquelas que o mudam" (Steve Jobs)',
       about: 'Me chamo Jhonatan Teixeira Rios, e atuo como programador há 2 anos. Sou formado em comunicação visual e tenho 1 ano de experiência como Designer Gráfico e Motion Designer. Atualmente trabalho como Desenvolvedor Front-End para clientes externos além de manter sistemas web e aplicativos independentes, desenvolvidos em diversas técnologias como Vue, React, React Native, Node e Python.',
-      technologies: [
-        { 
-          title: 'HTML', 
-          url: './technologies/html5.png' 
-        }, 
-        { 
-          title: 'CSS', 
-          url: './technologies/css3.png' 
-        }, 
-        { 
-          title: 'JS', 
-          url: './technologies/js.png' 
-        }, 
-        { 
-          title: 'React', 
-          url: './technologies/react.png' 
-        }, 
-        { 
-          title: 'React Native', 
-          url: './technologies/rn.png' 
-        }, 
-        { 
-          title: 'Next.js', 
-          url: './technologies/next.png' 
-        }, 
-        { 
-          title: 'Vue', 
-          url: './technologies/vue.png' 
-        }, 
-        { 
-          title: 'Nuxt.js', 
-          url: './technologies/nuxt.png' 
-        }, 
-        { 
-          title: 'Node', 
-          url: './technologies/node.png' 
-        }, 
-        { 
-          title: 'Git', 
-          url: './technologies/git.png' 
-        }, 
-        { 
-          title: 'Sass', 
-          url: './technologies/styled.png' 
-        }, 
-        { 
-          title: 'Figma', 
-          url: './technologies/figma.png' 
-        }, 
-        { 
-          title: 'Adobe Creative Suite', 
-          url: './technologies/adobe.png' 
-        }, 
-        { 
-          title: 'MongoDB', 
-          url: './technologies/mongo.png' 
-        }, 
-        { 
-          title: 'MySQL', 
-          url: './technologies/mysql.png' 
-        }, 
-        { 
-          title: 'Python', 
-          url: './technologies/python.png' 
-        }, 
-        {
-          title: 'Flask',
-          url: './technologies/flask.png'
-        }
-      ],
+      technologies: {
+        frontend: [
+          { 
+            title: 'HTML', 
+            url: './technologies/html5.png' 
+          }, 
+          { 
+            title: 'CSS', 
+            url: './technologies/css3.png' 
+          }, 
+          { 
+            title: 'JS', 
+            url: './technologies/js.png' 
+          }, 
+          { 
+            title: 'React', 
+            url: './technologies/react.png' 
+          },
+          { 
+            title: 'Redux', 
+            url: './technologies/redux.png' 
+          },
+          { 
+            title: 'React Native', 
+            url: './technologies/rn.png' 
+          }, 
+          { 
+            title: 'Next.js', 
+            url: './technologies/next.png' 
+          }, 
+          { 
+            title: 'Vue', 
+            url: './technologies/vue.png' 
+          }, 
+          { 
+            title: 'Nuxt.js', 
+            url: './technologies/nuxt.png' 
+          }, 
+          { 
+            title: 'Sass', 
+            url: './technologies/sass.png' 
+          },
+          { 
+            title: 'Styled-Components', 
+            url: './technologies/styled.png' 
+          },
+          { 
+            title: 'Figma', 
+            url: './technologies/figma.png' 
+          }, 
+          { 
+            title: 'Adobe Creative Suite', 
+            url: './technologies/adobe.png' 
+          }
+        ],
+        backend: [
+          { 
+            title: 'Node', 
+            url: './technologies/node.png' 
+          },
+          { 
+            title: 'Express', 
+            url: './technologies/express.png' 
+          },
+          { 
+            title: 'Gulp', 
+            url: './technologies/gulp.png' 
+          },
+          { 
+            title: 'Git', 
+            url: './technologies/git.png' 
+          }, 
+          { 
+            title: 'Python', 
+            url: './technologies/python.png' 
+          }, 
+          {
+            title: 'Flask',
+            url: './technologies/flask.png'
+          },
+          { 
+            title: 'MongoDB', 
+            url: './technologies/mongo.png' 
+          }, 
+          { 
+            title: 'MySQL', 
+            url: './technologies/mysql.png' 
+          },
+          { 
+            title: 'RealmDB', 
+            url: './technologies/realmdb.png' 
+          }
+        ]
+      },
       github: 'Compartilhar o conhecimento e a experiência nos ajuda a ir mais longe. Acesse aqui o código de meus principais projetos.'
     }
   }
--- partials/Home/Technologies/index.js
+++ partials/Home/Technologies/index.js
@@ -1,37 +1,13 @@
 import styles from './styles.module.scss'
-import TechnologyCircle from '../../../components/Home/TechnologyCircle'
-import Flickity from 'react-flickity-component'
+import TechnologiesCarousel from '../../../components/Home/TechnologiesCarousel'
 
 export default function Technologies({content}){
-
-  const flickityOptions = {
-    initialIndex: 0,
-    draggable: true,
-    contain: true,
-    prevNextButtons: false,
-    pageDots: false,
-  }
 
   return (
     <section className={styles.container} id={"home-technologies"} >
       <h2 className={`${styles.title} title`}> Linguagens e Tecnologias </h2>
-        <div className={styles.carousel}>
-          <Flickity
-            className={'carousel'} 
-            elementType={'div'} 
-            options={flickityOptions} 
-            reloadOnUpdate 
-            static
-          >
-            {
-              content.map((item, index) => {
-                return(
-                  <TechnologyCircle item={item} key={index} />
-                )
-              })
-            }
-          </Flickity>
-        </div>
+      <TechnologiesCarousel content={content.frontend} title={"Front-End"} />
+      <TechnologiesCarousel content={content.backend} title={"Back-End"} />
     </section> 
   )
 }
