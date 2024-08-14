a6843fc5ee035644a7fd726ffeb5ff5679de62cc
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-08-14 01:56:04 -0300

    updating projects

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const content = {
   },
   body: {
     hero: '"The ones who are crazy enough to think they can change the world are the ones who do" (Steve Jobs)',
-    about: "My name is Jhonatan Teixeira Rios and I have been working with technology since 2016. I have a technical degree in visual communication and worked as a Graphic Designer and Motion Designer for 1 year. After that, I made a career transition to Software Development, when I started working as a Frontend Engineer at the company Cielo, working directly on the Landing Page, with technologies such as Handlebars, JQuery, Vue, Nuxt, Scss and Webpack. I currently work as a Data Engineer at PagSeguro, creating internal data solutions for ETL (extraction, transformation and loading), Data Pipelines and data analysis, using technologies such as Typescript, Python, SQL, Kafka, RabbitMQ, Docker, Kubernetes and AWS. I also have some personal projects such as my blog, applications and games for Android and Artificial Intelligence projects. Additionally, I have been specializing in cloud development with AWS and recently acquired an AWS Cloud Practitioner certification.",
+    about: "My name is Jhonatan Teixeira Rios and I have been working with technology since 2016. I have a technical degree in visual communication and worked as a Graphic Designer and Motion Designer for 3 years. After that, I made a career transition to Software Development, when I started working as a Frontend Engineer at the company Cielo, working directly on the Landing Page, that was firstly made in Handlebars, JQuery and Bootstrap, and after some months we rewrite using Vue, Nuxt, Scss and Bulma. I currently work as a Data Engineer at PagSeguro, creating internal data solutions for ETL (extraction, transformation and loading), Data Pipelines and data analysis, using technologies such as Typescript, Python, SQL, Kafka, RabbitMQ, Docker, Kubernetes and AWS. I also have some personal projects such as my blog, applications and games for Android and Artificial Intelligence projects. Additionally, I have been specializing in cloud development with AWS and recently acquired an AWS Cloud Practitioner certification.",
     technologies: {
       frontend: [
         {
@@ -184,6 +184,14 @@ const content = {
       title: 'Check out my main projects',
       projectList: [
         {
+          title: 'Blog',
+          description: 'My personal blog is a set of content that I considered important during my studies, so I decided to organize my knowledge into articles about careers, programming, AWS and architecture. This blog was created with Next.js, Scss, Node, Typescript, MongoDB, Vercel and AWS.',
+          background: './projects/blog.png',
+          link: 'https://jhonatan-dev-rios-blog.vercel.app/',
+          github: 'https://github.com/jhonatan98rios/blog-pessoal',
+          color: 'rgba(14,179,156, .9)'
+        },
+        {
           title: 'Classifiers',
           description: 'This project was created while my studies about microservices and AWS. This project consists in 3 microservices that works receiving a csv file with the train data, realizing the train of classification model and providing a endpoint to consume the trained model as a Rest API. For this project I decided to use Bun.js as a runtime with Typescript, AWS S3, AWS SQS and I intend to deploy with Kuberntes on AWS too.',
           background: './projects/classifiers.png',
@@ -191,44 +199,44 @@ const content = {
           color: 'rgba(91,26,184, .9)'
         },
         {
-          title: 'Blog',
-          description: 'My personal blog is a set of content that I considered important during my studies, so I decided to organize my knowledge into articles about careers, programming, AWS and architecture. This blog was created with Next.js, Scss, Node, Typescript, MongoDB, Vercel and AWS.',
-          background: './projects/blog.png',
-          link: 'https://blog.jhonatan-dev-rios.com/',
-          github: 'https://github.com/jhonatan98rios/blog-pessoal',
+          title: 'Metal Against Demons',
+          description: "This is project is a 2D top down game, made with Java and Kotlin. It's inspired by Vampire Survivors, but the objective is to defeat monsters using music.",
+          background: './projects/mad.png',
+          link: 'https://play.google.com/store/apps/details?id=com.teixeirarios.mad',
+          github: 'https://github.com/jhonatan98rios/metal-against-demons-android',
           color: 'rgba(14,179,156, .9)'
-        },
-        {
-          title: 'Dead Nation',
-          description: 'Dead Nation is a 2D top-down zombie shooter, with different difficulty levels and a store with weapons and equipment to improve your character. The technologies used were: Ionic, React, Typescript and Sass',
-          background: './projects/dead-nation.png',
-          link: 'https://play.google.com/store/apps/details?id=com.rios.deadnation',
-          github: 'https://github.com/jhonatan98rios/DeadNationApp',
-          color: 'rgba(91,26,184, .9)'
         },
         {
           title: 'Next Ecommerce',
-          description: 'This project was created with the aim of serving as a front-end for an ecommerce based on microservices, but it is not yet finished.',
+          description: "This project was created with the aim of serving as a front-end for an ecommerce based on microservices, but it's not finished yet.",
           background: './projects/ecommerce.png',
           link: 'https://jhonatan-dev-rios-store.vercel.app/',
           github: 'https://github.com/jhonatan98rios/nextjs-commerce',
-          color: 'rgba(14,179,156, .9)'
+          color: 'rgba(91,26,184, .9)'
         },
         {
           title: 'Swordio',
           description: 'Swordio is a Turn-based online RPG, PVP combat that runs directly in the browser. Just create an account, log in and create a room. Then just share the room with your friends and start playing. The technologies used were: Vue, Sass, Node, Socket.io, PostgreSQL',
           background: './projects/swordio.png',
           github: 'https://github.com/jhonatan98rios/swordio',
-          color: 'rgba(91,26,184, .9)'
-        },
-        {
-          title: 'Guia de hardware (Deprecated)',
-          description: "This app uses artificial intelligence and natural language processing to recommend notebook models, based on the user's needs. The technologies used were: React-Native, Redux, Redux-saga, Styled-components, Node, Express, Natural.js, Jest and MongoDB",
-          background: './projects/hardware-guide.png',
-          link: 'https://play.google.com/store/apps/details?id=com.hardwareguide',
-          github: 'https://github.com/jhonatan98rios/HardwareGuide',
           color: 'rgba(14,179,156, .9)'
         },
+        // {
+        //   title: 'Dead Nation',
+        //   description: 'Dead Nation is a 2D top-down zombie shooter, with different difficulty levels and a store with weapons and equipment to improve your character. The technologies used were: Ionic, React, Typescript and Sass',
+        //   background: './projects/dead-nation.png',
+        //   link: 'https://play.google.com/store/apps/details?id=com.rios.deadnation',
+        //   github: 'https://github.com/jhonatan98rios/DeadNationApp',
+        //   color: 'rgba(91,26,184, .9)'
+        // },
+        // {
+        //   title: 'Guia de hardware (Deprecated)',
+        //   description: "This app uses artificial intelligence and natural language processing to recommend notebook models, based on the user's needs. The technologies used were: React-Native, Redux, Redux-saga, Styled-components, Node, Express, Natural.js, Jest and MongoDB",
+        //   background: './projects/hardware-guide.png',
+        //   link: 'https://play.google.com/store/apps/details?id=com.hardwareguide',
+        //   github: 'https://github.com/jhonatan98rios/HardwareGuide',
+        //   color: 'rgba(14,179,156, .9)'
+        // },
       ],
     },
     github: 'Sharing knowledge and experience helps us go further. Access the code of my main projects here.',
