4b44fffde1d1a20806467b726bc637e746fb9cee
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-08-17 00:50:23 -0300

    updating projects and stacks

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,36 @@ const content = {
     hero: '"The ones who are crazy enough to think they can change the world are the ones who do" (Steve Jobs)',
     about: "My name is Jhonatan Teixeira Rios and I have been working with technology since 2016. I have a technical degree in visual communication and worked as a Graphic Designer and Motion Designer for 3 years. After that, I made a career transition to Software Development, when I started working as a Frontend Engineer at the company Cielo, working directly on the Landing Page, that was firstly made in Handlebars, JQuery and Bootstrap, and after some months we rewrite using Vue, Nuxt, Scss and Bulma. I currently work as a Data Engineer at PagSeguro, creating internal data solutions for ETL (extraction, transformation and loading), Data Pipelines and data analysis, using technologies such as Typescript, Python, SQL, Kafka, RabbitMQ, Docker, Kubernetes and AWS. I also have some personal projects such as my blog, applications and games for Android and Artificial Intelligence projects. Additionally, I have been specializing in cloud development with AWS and recently acquired an AWS Cloud Practitioner certification.",
     technologies: {
+      languages: [
+        {
+          title: 'HTML',
+          url: './technologies/languages/html5.png'
+        },
+        {
+          title: 'CSS',
+          url: './technologies/languages/css3.png'
+        },
+        {
+          title: 'Javascript',
+          url: './technologies/languages/js.png'
+        },
+        {
+          title: 'Typescript',
+          url: './technologies/languages/typescript.png'
+        },
+        {
+          title: 'Python',
+          url: './technologies/languages/python.png'
+        },
+        {
+          title: 'Java',
+          url: './technologies/languages/java.png'
+        },
+        {
+          title: 'Kotlin',
+          url: './technologies/languages/kotlin.png'
+        },
+      ],
       frontend: [
         {
           title: 'React',
@@ -32,22 +62,6 @@ const content = {
           url: './technologies/frontend/nuxt.png'
         },
         {
-          title: 'Typescript',
-          url: './technologies/frontend/typescript.png'
-        },
-        {
-          title: 'Javascript',
-          url: './technologies/frontend/js.png'
-        },
-        {
-          title: 'HTML',
-          url: './technologies/frontend/html5.png'
-        },
-        {
-          title: 'CSS',
-          url: './technologies/frontend/css3.png'
-        },
-        {
           title: 'Redux',
           url: './technologies/frontend/redux.png'
         },
@@ -55,7 +69,11 @@ const content = {
           title: 'React Native',
           url: './technologies/frontend/rn.png'
         },
-        
+      
+        {
+          title: 'Jetpack Compose',
+          url: './technologies/frontend/jetpack.png'
+        },
         {
           title: 'Sass',
           url: './technologies/frontend/sass.png'
@@ -83,12 +101,12 @@ const content = {
           url: './technologies/backend/nest.png'
         },
         {
-          title: 'Python',
-          url: './technologies/backend/python.png'
-        },
-        {
           title: 'Flask',
           url: './technologies/backend/flask.png'
+        },
+        {
+          title: 'Spring',
+          url: './technologies/backend/spring.png'
         },
         {
           title: 'GraphQL',
@@ -215,12 +233,20 @@ const content = {
           color: 'rgba(91,26,184, .9)'
         },
         {
-          title: 'Swordio',
-          description: 'Swordio is a Turn-based online RPG, PVP combat that runs directly in the browser. Just create an account, log in and create a room. Then just share the room with your friends and start playing. The technologies used were: Vue, Sass, Node, Socket.io, PostgreSQL',
-          background: './projects/swordio.png',
-          github: 'https://github.com/jhonatan98rios/swordio',
+          title: 'Guia de Hardware Web',
+          description: "This app uses artificial intelligence and natural language processing to recommend notebook models, based on the user's needs. The technologies used were: Next 14, Tailwind, Typescript, Serverless Framework and Natural.js",
+          background: './projects/hardware-guide.png',
+          link: 'https://hardware-guide-web.vercel.app',
+          github: 'https://github.com/jhonatan98rios/hardware-guide-web',
           color: 'rgba(14,179,156, .9)'
         },
+        // {
+        //   title: 'Swordio',
+        //   description: 'Swordio is a Turn-based online RPG, PVP combat that runs directly in the browser. Just create an account, log in and create a room. Then just share the room with your friends and start playing. The technologies used were: Vue, Sass, Node, Socket.io, PostgreSQL',
+        //   background: './projects/swordio.png',
+        //   github: 'https://github.com/jhonatan98rios/swordio',
+        //   color: 'rgba(14,179,156, .9)'
+        // },
         // {
         //   title: 'Dead Nation',
         //   description: 'Dead Nation is a 2D top-down zombie shooter, with different difficulty levels and a store with weapons and equipment to improve your character. The technologies used were: Ionic, React, Typescript and Sass',
--- partials/Home/Technologies/index.js
+++ partials/Home/Technologies/index.js
@@ -6,6 +6,7 @@ export default function Technologies({content}){
   return (
     <section className={styles.container} id={"technologies"} >
       <h2 className={`${styles.title} title`}> Programming Languages and Technologies </h2>
+      <TechnologiesCarousel content={content.languages} title={"Languages"} />
       <TechnologiesCarousel content={content.frontend} title={"Front-End"} />
       <TechnologiesCarousel content={content.backend} title={"Back-End"} />
       <TechnologiesCarousel content={content.infra} title={"Infrastructure"} />
