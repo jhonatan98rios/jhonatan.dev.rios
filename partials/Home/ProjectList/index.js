350582ab8b4d2121734cfb98803d2919cadf65a9
jhonatan98rios <jhonatan98rios@gmail.com>
2026-08-30 00:57:33 -0300

    fix: stop carousel clipping scaled card top

--- partials/Home/ProjectList/index.js
+++ partials/Home/ProjectList/index.js
@@ -51,7 +51,7 @@ export default function ProjectList({ content }) {
 
           <div
             ref={trackRef}
-            className="flex snap-x snap-mandatory gap-4 overflow-x-auto scroll-smooth pb-2 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
+            className="flex snap-x snap-mandatory gap-4 overflow-x-auto scroll-smooth pt-3 pb-2 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden"
           >
             {projects.map((project, i) => (
               <ProjectCard key={i} card={project} active={i === selected} onSelect={() => setSelected(i)} />
